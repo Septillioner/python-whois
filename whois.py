9692c39e28894b7ea0162f7238827380735e9878
Septillioner <egeismailkosedag@gmail.com>
2017-09-29 22:17:44 +0300

    Add files via upload

---
+++ whois.py
@@ -0,0 +1,306 @@
+whois_servers =""".yu	whois.ripe.net
+.yt	whois.nic.yt
+.ws	whois.nic.ws
+.wf	whois.nic.wf
+.vg	whois.adamsnames.tc
+.ve	whois.nic.ve
+.vc	whois2.afilias-grs.net
+.va	whois.ripe.net
+.uz	whois.cctld.uz
+.uy	whois.nic.org.uy
+.us	whois.nic.us
+.uk	whois.nic.uk
+.ug	whois.co.ug
+.ua	whois.net.ua
+.tw	whois.twnic.net
+.tv	tvwhois.verisign-grs.com
+.travel	whois.nic.travel
+.tr	www.nic.tr
+.to	whois.tonic.to
+.tn	whois.ripe.net
+.tm	whois.nic.tm
+.tl	whois.nic.tl
+.tk	whois.dot.tk
+.tj	whois.nic.tj
+.th	whois.thnic.net
+.tf	whois.nic.fr
+.tel	whois-tel.neustar.biz
+.tc	whois.adamsnames.tc
+.su	whois.ripn.net
+.st	whois.nic.st
+.so	whois.nic.so
+.sn	whois.nic.sn
+.sm	whois.ripe.net
+.sk	whois.sk-nic.sk
+.si	whois.arnes.si
+.sh	whois.nic.sh
+.sg	eos.nic.net.sg
+.se	whois01.prod.iis.se
+.sc	whois2.afilias-grs.net
+.sb	whois.nic.net.sb
+.sa	nic.net.sa
+.ru	whois.ripn.net
+.rs	whois.rnids.rs
+.ro	whois.rotld.ro
+.re	whois.nic.fr
+.pt	hercules.dns.pt
+.pro	whois.registrypro.pro
+.pr	whois.nic.pr
+.pm	whois.nic.pm
+.pl	whois.dns.pl
+.pe	kero.yachay.pe
+.org.au	whois.ausregistry.net.au
+.org	whois.publicinterestregistry.net
+.nz	srs-ak.srs.net.nz
+.nu	whois.worldnames.net
+.no	whois.norid.no
+.nl	whois.domain-registry.nl
+.net.au	whois.ausregistry.net.au
+.net	whois.verisign-grs.com
+.name	whois.nic.name
+.na	whois.na-nic.com.na
+.my	whois.mynic.net.my
+.mx	whois.nic.mx
+.museum	whois.museum
+.mu	whois.nic.mu
+.mt	whois.ripe.net
+.ms	whois.nic.ms
+.mobi	whois.dotmobiregistry.net
+.mn	whois.nic.mn
+.mk	whois.ripe.net
+.mil	whois.nic.mil
+.mg	whois.nic.mg
+.me	whois.meregistry.net
+.md	whois.nic.md
+.mc	whois.ripe.net
+.ma	whois.iam.net.ma
+.ly	whois.nic.ly
+.lv	whois.ripe.net
+.lu	whois.dns.lu
+.lt	www.whois.lt
+.li	whois.nic.ch
+.la	whois.nic.la
+.kz	whois.nic.kz
+.kr	whois.krnic.net
+.kp	whois.kcce.kp
+.ki	whois.nic.ki
+.kh	whois.nic.net.kh
+.kg	whois.domain.kg
+.ke	whois.kenic.or.ke
+.jp	whois.jprs.jp
+.jobs	jobswhois.verisign-grs.com
+.jo	whois.ripe.net
+.je	whois.je
+.it	whois.nic.it
+.is	min.isnic.is
+.ir	whois.nic.ir
+.io	whois.nic.io
+.int	whois.iana.org
+.info	whois.afilias.net
+.in	whois.inregistry.net
+.im	whois.nic.im
+.il	register.isoc.org.il
+.ie	whois.domainregistry.ie
+.id.au	whois.ausregistry.net.au
+.hu	whois.nic.hu
+.ht	whois.nic.ht
+.hr	whois.ripe.net
+.hn	whois2.afilias-grs.net
+.hm	webhost1.capital.hm
+.hk	whois.hkdnr.net.hk
+.gy	whois.registry.gy
+.gs	whois.cocca.cx
+.gr	whois.ripe.net
+.gov.uk	whois.ja.net
+.gov	whois.dotgov.gov
+.gm	whois.ripe.net
+.gl	whois.ripe.net
+.gi	whois2.afilias-grs.net
+.gg	whois.gg
+.ge	whois.ripe.net
+.gd	whois.adamsnames.com
+.gb	whois.ripe.net
+.ga	whois.ripe.net
+.fr	whois.nic.fr
+.fo	whois.ripe.net
+.fi	whois.ficora.fi
+.eu	whois.eu
+.es	whois.ripe.net
+.eg	whois.ripe.net
+.ee	myristaja.eenet.ee
+.edu	whois.educause.net
+.ec	whois.nic.ec
+.dz	whois.ripe.net
+.do	ns.nic.do
+.dm	whois.nic.dm
+.dk	vocal.dk-hostmaster.dk
+.de	whois.denic.de
+.cz	whois.nic.cz
+.cy	whois.ripe.net
+.cx	whois.nic.cx
+.coop	whois.nic.coop
+.com.au	whois.ausregistry.net.au
+.com	whois.verisign-grs.com
+.co.za	whois.coza.net.za
+.cn	whois.cnnic.net.cn
+.cl	nic.cl
+.ck	whois.ck-nic.org.ck
+.ci	whois.nic.ci
+.ch	whois.nic.ch
+.cd	whois.nic.cd
+.cc	ccwhois.verisign-grs.com
+.cat	whois.cat
+.ca	whois.cira.ca
+.bz	bzwhois.verisign-grs.co
+.by	whois.cctld.by
+.br	whois.nic.br
+.bo	whois.nic.bo
+.bj	whois.nic.bj
+.biz	whois.biz
+.bg	www.register.bg
+.be	whois.dns.be
+.ba	whois.ripe.net
+.az	whois.ripe.net
+.au	whois.ausregistry.net
+.at	whois.nic.at
+.asn.au	whois.ausregistry.net.au
+.asia	whois.dotasia.net
+.as	whois.nic.as
+.arpa	whois.iana.org
+.am	whois.amnic.net
+.al	whois.ai
+.ai	whois.ai
+.ag	whois2.afilias-grs.net
+.af	whois.nic.af
+.aero	whois.aero
+.ae	whois.aeda.net.ae
+.ad	whois.ripe.net
+.ac.uk	whois.ja.net
+.ac	whois.nic.ac"""
+servers = {}
+import socket
+import argparse
+import sys
+import datetime
+
+for item in whois_servers.split("\n"):
+	tac = item.split("\t")
+	servers[tac[0]] = tac[1]
+def setupHost(domain,port):
+	try:
+		return (socket.gethostbyname(domain),port)
+	except:
+		return ("whois.verisign-grs.com",port)
+def getWNS(domain_extend):
+	global servers
+	try:
+		return servers[domain_extend]
+	except:
+		return "SNF"
+def whois_query(domain):
+	if(len(domain.split("www.")) > 1):
+		domain = domain.split("www.")[1]
+	if( len(domain.split(".")) > 1): 
+		server = getWNS(".%s"%(".".join(domain.split(".")[1:])))
+		if(server != "SNF"):
+			host = setupHost(server,43)
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			sock.settimeout(4)
+			try:
+				sock.connect(host)
+				sock.send("%s\r\n"%(domain))
+				return sock.recv(8192)
+			except:
+				return "Connection Error!"
+			
+		else:
+			pass
+	else:
+		raise NameError("Not Found")
+def create_list(str_domain):
+	domain_header = str_domain.split(";")[0].split(".")[0]
+	main_domain = ".".join(str_domain.split(";")[0].split(".")[0:])
+	query_list = [main_domain]
+	try:
+		extend_list = str_domain.split(";")[1:]
+		for n in extend_list:
+			query_list.append("%s.%s"%(domain_header,n))
+	except:
+		pass
+	return query_list
+def create_allofthem(str_domain):
+	global servers
+	domain_header = str_domain.split(".")[0]
+	dlist = []
+	for extend in servers:
+		dlist.append("%s%s"%(domain_header,extend))
+	return dlist
+def showUsage():
+	print "Usage whois:\n\twhois.py -domain example.com\n\twhois.py -domain example.com;org;net\n\twhois.py -domain example.com --allofthem\n\twhois.py -domain example.com -save example.txt\n\twhois.py -domain example.com -save C:\\example.txt"
+query_log = []
+def loadQuery(query_result,domain):
+	global query_log
+	query_log.append((query_result,domain))
+def createLoadBar(queryTotal,completedQuery,queryDomain,queryServer):
+	return "[{}{}] {}% {}/{} {} => {}       \r".format("#"*int((20.0/queryTotal)*completedQuery),
+												"-"*((20-int((20.0/queryTotal)*completedQuery))),
+												int((100.0/queryTotal)*completedQuery),
+												completedQuery,
+												queryTotal,
+												queryDomain,
+												queryServer
+												)
+
+def main():
+	global query_log
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-domain", help="Whois query.")
+	parser.add_argument("--allofthem", help="Whois query.",action="store_true")
+	parser.add_argument("-save",help = "Saves Queries.")
+	args = parser.parse_args()
+	try:
+		if(args.allofthem):
+			if(len(args.domain.split(";")) == 1):
+				domain_list = create_allofthem(args.domain)
+				queryTotal = len(domain_list)
+				for item in domain_list:
+					query_result = whois_query(item)
+					loadQuery(query_result,item)
+					try:
+						sys.stdout.write(createLoadBar(queryTotal,len(query_log),item,socket.gethostbyname(item)))
+					except:
+						sys.stdout.write(createLoadBar(queryTotal,len(query_log),item,"Not declared!"))
+					sys.stdout.flush()
+				sf = "dataLog%s.txt"%(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+				with open(sf,"wb") as fp:
+					fp.write("Logged At %s"%(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+					fp.write("--------LOG STARTED--------")	
+					for query in query_log:
+						fp.write("WHOIS QUERY's %s:\n\t"%(query[1]))
+						fp.write(query[0].replace("\n","\n\t"))
+						fp.write("--------------------")
+				print "\nSaved on %s"%(sf)
+			else:
+				raise NameError("Wrong Using!!")
+		else:
+			domain_list = create_list(args.domain)
+			for item in domain_list:
+				print "Asking %s"%(item)
+				query_result = whois_query(item)
+				loadQuery(query_result,item)
+				print "WHOIS Response : \n\t%s"%(query_result.replace("\n","\n\t"))
+	except:
+		showUsage()
+		sys.exit()
+	if(args.save != None):
+		with open(args.save,"wb") as fp:
+			fp.write("Logged At %s"%(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+			fp.write("--------LOG STARTED--------")	
+			for query in query_log:
+				fp.write("WHOIS QUERY's %s:\n\t"%(query[1]))
+				fp.write(query[0].replace("\n","\n\t"))
+				fp.write("--------------------")
+		print "Saved on %s"%(args.save)
+
+if __name__ == '__main__':
+	main()
